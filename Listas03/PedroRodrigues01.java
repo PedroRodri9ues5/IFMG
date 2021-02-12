d608fde0a2aed0b62970e9e650c3122296520162
PedroRodri9ues5 <70603927+PedroRodri9ues5@users.noreply.github.com>
2021-02-12 09:30:33 -0300

    Delete Listas03 directory

--- Listas03/PedroRodrigues01.java
+++
@@ -1,7 +0,0 @@
-public class PedroRodrigues01 {
-    public static void main(String[] args) {
-        for (int i = 1 ; i <= 100 && i >= 0; i++){
-            System.out.println(i);
-        }
-    }
-}
--- Listas03/PedroRodrigues03.java
+++
@@ -1,7 +0,0 @@
-public class PedroRodrigues03 {
-    public static void main(String[] args) {
-        for (int i = 100 ; i <= 100 && i > 0; i--){
-            System.out.println(i);
-        }
-    }
-}
